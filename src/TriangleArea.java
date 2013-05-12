3b7153c9e3902714855dae1e9f1e92a5120e5c7c
akihiro <akihiro@192.168.1.68>
2013-05-12 16:56:03 +0900

    チェック処理追加

--- src/TriangleArea.java
+++ src/TriangleArea.java
@@ -11,27 +11,49 @@ import java.io.InputStreamReader;
 public class TriangleArea {
 
 	public static void main(String args[]) throws NumberFormatException, IOException {
-		double a, b, c, s, sq, S;
-		
 		BufferedReader input =
 	            new BufferedReader (new InputStreamReader (System.in));	
 		
 		System.out.println("三角形の面積を求めるプログラム開始 ");
 		System.out.println("1辺の長さを入力してください: ");
-		a = Double.parseDouble(input.readLine());
+		String stringA = String.valueOf(input.readLine());
+		checkNum(stringA);
+		
 		System.out.println("1辺の長さを入力してください: ");
-		b = Double.parseDouble(input.readLine());
+		String stringB = String.valueOf(input.readLine());
+		checkNum(stringB);
+		
 		System.out.println("1辺の長さを入力してください: ");
-		c = Double.parseDouble(input.readLine());
+		String stringC = String.valueOf(input.readLine());
+		checkNum(stringC);
+		
+		displayTriangleArea(stringA, stringB, stringC);
+		
+	}
+	
+	private static void checkNum(String num) {
+		
+		if (num == null || !num.matches("^[0-9¥.]+$")) {
+			// 半角数字に一致しない場合、エラー
+			System.out.println("数値でない値が入力されています");
+		    System.exit(0);
+		}
+	}
+	
+	private static void displayTriangleArea(String stringA, String stringB, String stringC){
+		
+		double a = Double.parseDouble(stringA);
+		double b = Double.parseDouble(stringB);
+		double c = Double.parseDouble(stringC);
 		
 		// 1番長い辺が、他の２辺を足した値より小さい
-		s = (a + b + c) / 2;
-		sq = s * (s - a) * (s - b) * (s - c);
+		double s = (a + b + c) / 2;
+		double sq = s * (s - a) * (s - b) * (s - c);
 		
 		if (sq <= 0) {
 			System.out.print("三角形になりません\n");
 		} else {
-			S = Math.sqrt(sq);
+			double S = Math.sqrt(sq);
 			System.out.printf("面積は%fです\n", S);
 		}
 	}
